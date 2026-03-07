3fe8453b7219d6e561632303d2f671b8751b93ec
Andy Edwards <jedwards@fastmail.com>
2026-03-07 00:04:02 -0600

    fix: argh, remove ESM for now

--- toolchain.config.cjs
+++ toolchain.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   cjsBabelEnv: { targets: { node: 16 } },
   esmBabelEnv: { targets: { node: 16 } },
   // outputCjs: false, // disables CJS output (default: true)
-  // outputEsm: false, // disables ESM output (default: true)
+  outputEsm: false, // disables ESM output (default: true)
   buildIgnore: ['src/**/*.spec.ts'],
   hasTypeScriptSources: true,
   // esWrapper: true, // outputs ES module wrappers for CJS modules (default: false)
